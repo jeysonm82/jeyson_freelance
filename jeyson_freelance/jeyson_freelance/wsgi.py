5a1ae44eb959ac269bbd7cfb77b0dcaca9de6203
jeysonmc <jeyson.molina.co@gmail.com>
2016-07-19 10:32:32 -0500

    Initial commit. Base django project.

---
+++ jeyson_freelance/jeyson_freelance/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jeyson_freelance project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jeyson_freelance.settings")
+
+application = get_wsgi_application()
