791b8d564547cfa7dc23869b12557a4faeb888cf
jeysonmc <jeyson.molina.co@gmail.com>
2016-07-20 10:49:29 -0500

    Initial implementation for Bio and Skill models.

--- jeyson_freelance/web_app/models.py
+++ jeyson_freelance/web_app/models.py
@@ -3,3 +3,26 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Bio(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    description = models.TextField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    location = models.CharField(max_length=30)
+    latlong = models.CharField(max_length=50)
+    github_url = models.URLField("Github URL")
+    linkedin_url = models.URLField("Linkedin URL")
+
+
+class SkillCategory(models.Model):
+    name = models.CharField("Name", max_length=30)
+    order = models.SmallIntegerField(default = 0)
+
+    class Meta:
+        ordering = ['order']
+
+class Skill(models.Model):
+    name = models.CharField("Name", max_length=30)
+    description = models.TextField()
+    category = models.ForeignKey(SkillCategory)
