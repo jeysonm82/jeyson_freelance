dcbf3870319bd7bfd9800b5e472f478dc5c3ffe9
jeysonmc <jeyson.molina.co@gmail.com>
2016-07-20 11:03:43 -0500

    Initial implementation for Experience a Project models.

--- jeyson_freelance/web_app/models.py
+++ jeyson_freelance/web_app/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from versatileimagefield.fields import VersatileImageField, PPOIField
 
 # Create your models here.
 class Bio(models.Model):
@@ -26,3 +27,22 @@ class Skill(models.Model):
     name = models.CharField("Name", max_length=30)
     description = models.TextField()
     category = models.ForeignKey(SkillCategory)
+
+
+class Experience(models.Model):
+    title = models.CharField(max_length=70)
+    _from = models.DateField()
+    to = models.DateTimeField(blank=True, null=True)
+    description = models.TextField()
+    skills = models.ManyToManyField(Skill)
+    exp_type = models.CharField("Experience type", max_length=10, choices=[('education', 'Education'), ('work', 'Work')])
+
+class Projects(models.Model):
+    title = models.CharField(max_length=70)
+    short_desc = models.CharField(max_length=140)
+    image = VersatileImageField('Imagen', ppoi_field='ppoi')
+    ppoi = PPOIField('Image center')
+    description = models.TextField()
+    demo_url = models.URLField("Demo URL", blank=True, null=True)
+    source_url = models.URLField("Source URL", blank=True, null=True)
+    skills = models.ManyToManyField(Skill)
