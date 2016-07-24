da31c8bf7e2ccd26648f17ba5ac2f1459e07f961
jeysonmc <jeyson.molina.co@gmail.com>
2016-07-24 17:35:57 -0500

    Homeview backend logic

--- jeyson_freelance/web_app/views.py
+++ jeyson_freelance/web_app/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from models import SkillCategory, Skill, Project
 
 class HomeView(TemplateView):
     template_name = 'web_app/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['skills'] = SkillCategory.objects.all()
+        context['sample_projects'] = list(Project.objects.all() )* 10
+        return context
